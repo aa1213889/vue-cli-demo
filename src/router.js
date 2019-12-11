46a95bbcf4a17574d21e9f55b68d190a124a7f26
unknown <jiangzhi@BRX.COM>
2019-12-11 19:04:46 +0800

    First commit.

---
+++ src/router.js
@@ -0,0 +1,21 @@
+
+// import Vue from 'vue'
+// import Router from 'vue-router' // 引入vue-router
+// import Page2 from './pages/page2' // 引入组件方式2
+
+// Vue.use(Router) //使用vue-router
+
+// export default new Router({
+//     routes: [
+//         {
+//             path: "/page1",
+//             name: "page1",
+//             component: () => import('./pages/page1') // 引入组件方式1
+//         },
+//         {
+//             path: '/page2',
+//             name: 'page2',
+//             component: Page2 // 引入组件方式2
+//         },
+//     ]
+// })
