2259a2d55e10f697306725ee2ae490e86c794ac7
unknown <jiangzhi@BRX.COM>
2019-12-12 18:56:47 +0800

    test1

--- src/router.js
+++ src/router.js
@@ -1,21 +1,16 @@
 
-// import Vue from 'vue'
-// import Router from 'vue-router' // 引入vue-router
-// import Page2 from './pages/page2' // 引入组件方式2
+import Vue from 'vue'
+import Router from 'vue-router' // 引入vue-router
 
-// Vue.use(Router) //使用vue-router
 
-// export default new Router({
-//     routes: [
-//         {
-//             path: "/page1",
-//             name: "page1",
-//             component: () => import('./pages/page1') // 引入组件方式1
-//         },
-//         {
-//             path: '/page2',
-//             name: 'page2',
-//             component: Page2 // 引入组件方式2
-//         },
-//     ]
-// })
+//组件模块
+import HelloWorld from './components/HelloWorld'
+
+
+Vue.use(Router) //使用vue-router
+
+export default new Router({
+    routes: [
+        { path: '/sb',  name: 'HelloWorld', component: HelloWorld}
+      ]
+})
